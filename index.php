cb48ecd55391f2d7047c46bb9849fadb6d5eff20
guevara282 <69832994+guevara282@users.noreply.github.com>
2024-09-23 17:15:44 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="flowers.png" type="image/x-icon">
+    <title>FLORES</title>
+</head>
+<body>
+    <div class="greetings">
+        <span>L</span>
+        <span>A</span>
+        <span>U</span>
+        <span>R</span>
+        <span>I</span>
+        <span>T</span>
+        <span>A</span>
+    </div>
+    <div class="greetings"></div>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <div class="description">
+        <span>tengo un regalito para tí!!</span>
+    </div>
+    <div class="button">
+        <button>
+            <a href="FLORES.html">Haz Clic</a>
+        </button>
+        
+    </div>
+</body>
+</html>
